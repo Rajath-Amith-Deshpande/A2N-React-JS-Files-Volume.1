43efe3a9f90895881484e1686458c93bcf904f15
Rajath.Amith.Deshpande <80612001+Rajath-Amith-Deshpande@users.noreply.github.com>
2021-04-20 08:13:23 +0530

    Add files via upload

---
+++ Class.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Classcomp extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return <div className="container-fluid">
+            <h1>HELLO</h1>
+        </div>
+    }
+}
+export default Classcomp;
